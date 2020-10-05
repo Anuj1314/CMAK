d5bac05d0e25a7b9aaa5bb21ca040b000ad4b2e0
Anuj Patel <anujpatel1314@gmail.com>
2020-10-05 12:34:17 +0530

    Update LogConfig.scala

--- app/kafka/manager/utils/zero90/LogConfig.scala
+++ app/kafka/manager/utils/zero90/LogConfig.scala
@@ -173,8 +173,8 @@ object LogConfig extends TopicConfigs {
     */
   def fromProps(defaults: java.util.Map[_ <: Object, _ <: Object], overrides: Properties): LogConfig = {
     val props = new Properties()
-    props.putAll(defaults.asInstanceOf[java.util.Map[_, _]])
-    props.putAll(overrides.asMap)
+    props.putAll(defaults)
+    props.putAll(overrides)
     LogConfig(props)
   }
 
